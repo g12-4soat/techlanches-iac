f1289190d34af03b22e25d2a33afa11cb909e8ec
Nicolas Soares <82896317+nsosoares@users.noreply.github.com>
2024-05-01 14:39:52 -0300

    Criado deployment de pagamento

--- k8s/apply-all.sh
+++ k8s/apply-all.sh
@@ -3,9 +3,10 @@
 # For Unix-like operating systems (Linux Distros, Mac OS ...)
 # /> chmod +x apply-all.sh
 # Apply all files (including subdirectories)
-kubectl apply -f ./k8s/metrics/metrics.yaml
-kubectl apply -f ./k8s/techlanches-namespace.yaml
-kubectl apply -f ./k8s/techlanches-secrets.yaml
-kubectl apply -f ./k8s/deployments/techlanches-api-pedido-deployment.yaml
-kubectl apply -f ./k8s/deployments/techlanches-worker-producao-deployment.yaml
-kubectl apply -f ./k8s/deployments/techlanches-rabbitmq-deployment.yaml
+kubectl apply -f ../k8s/metrics/metrics.yaml
+kubectl apply -f ../k8s/techlanches-namespace.yaml
+kubectl apply -f ../k8s/techlanches-secrets.yaml
+kubectl apply -f ../k8s/deployments/techlanches-api-pagamento-deployment.yaml
+kubectl apply -f ../k8s/deployments/techlanches-api-pedido-deployment.yaml
+kubectl apply -f ../k8s/deployments/techlanches-worker-producao-deployment.yaml
+kubectl apply -f ../k8s/deployments/techlanches-rabbitmq-deployment.yaml
