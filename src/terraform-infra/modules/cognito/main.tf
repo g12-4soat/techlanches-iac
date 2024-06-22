006974d2385f4de0958fe0cfd8df64058d19a0ae
Deamorim <deni.barbosa.amori@gmail.com>
2024-06-21 21:43:32 -0300

    Inclusão dos required attributes endereço e telefone.

--- src/terraform-infra/modules/cognito/main.tf
+++ src/terraform-infra/modules/cognito/main.tf
@@ -1,11 +1,35 @@
 resource "aws_cognito_user_pool" "tech_lanches_clientes_pool" {
   name                     = "tech-lanches-clientes-pool"
   mfa_configuration        = "OFF"
-  alias_attributes         = ["email", "preferred_username","phone_number","address"]
+  alias_attributes         = ["email", "preferred_username"]
   auto_verified_attributes = ["email"]
 
   username_configuration {
     case_sensitive = false
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name = "phone_number"
+    developer_only_attribute = false
+    mutable                  = true
+    required                 = true
+    string_attribute_constraints {
+      max_length = 14
+      min_length = 13
+    }
+  }
+
+  schema {
+    attribute_data_type = "String"
+    name = "address"
+    developer_only_attribute = false
+    mutable                  = true
+    required                 = true
+    string_attribute_constraints {
+      max_length = 80
+      min_length = 10
+    }
   }
 
   tags = {
