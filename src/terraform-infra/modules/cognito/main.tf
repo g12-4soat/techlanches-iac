35e92c3615c4039109980c95c1bb3ad68bd93e3c
Deamorim <deni.barbosa.amori@gmail.com>
2024-07-10 15:32:37 -0300

    Remoção dos schemas de endereço e telefone.

--- src/terraform-infra/modules/cognito/main.tf
+++ src/terraform-infra/modules/cognito/main.tf
@@ -8,30 +8,6 @@ resource "aws_cognito_user_pool" "tech_lanches_clientes_pool" {
     case_sensitive = false
   }
 
-  schema {
-    attribute_data_type = "String"
-    name = "phone_number"
-    developer_only_attribute = false
-    mutable                  = true
-    required                 = true
-    string_attribute_constraints {
-      max_length = 14
-      min_length = 13
-    }
-  }
-
-  schema {
-    attribute_data_type = "String"
-    name = "address"
-    developer_only_attribute = false
-    mutable                  = true
-    required                 = true
-    string_attribute_constraints {
-      max_length = 80
-      min_length = 10
-    }
-  }
-
   tags = {
     Name = "tech_lanches_clientes_pool"
   }
